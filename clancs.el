cab6fd0855cee77cd7f8b6a999044293e9d60edd
Amey Parulekar <amey@wirywolf.com>
2013-02-28 20:10:16 +0530

    Tried unsuccessfully to use emacs-deferred for asynchronous processing.

--- clancs.el
+++ clancs.el
@@ -1,17 +1,33 @@
 (require 'deferred (file-truename "emacs-deferred/deferred.el"))
 (require 'concurrent (file-truename "emacs-deferred/concurrent.el"))
+(require 'async "emacs-async/async.el")
 
-(pymacs-exec "from pyclang import sublimeclang")
-(pymacs-exec "from pyclang import sublime")
+(async-start
+   ;; What to do in the child process
+   (lambda ()
+     (message "This is a test")
+     (sleep-for 3)
+     222)
 
-(pymacs-exec "reload(sublimeclang)")
-(pymacs-exec "reload(sublime)")
+   ;; What to do when it finishes
+   (lambda (result)
+     (message "Async process done, result should be 222: %s" result)))
 
-(setq pyclang-scaa (pymacs-eval "sublimeclang.SublimeClangAutoComplete()"))
-(setq pymacs-forget-mutability t)
+(defun pyclang-init ()
+  (deferred:$
+    (deferred:next
+      (lambda ()
+	(pymacs-exec "from pyclang import sublimeclang")))
+    (deferred:nextc it
+      (lambda ()
+	(pymacs-exec "from pyclang import sublime")))
+    (deferred:nextc it
+      (lambda ()
+	(setq pymacs-forget-mutability t)
+	(setq pyclang-scaa (pymacs-eval "sublimeclang.SublimeClangAutoComplete()"))))))
 
 (defun pyclang-receive-completions (completions)
-  (message (prin1-to-string completions)))
+  (insert (prin1-to-string completions)))
 
 (defun pyclang-query-completions ()
   (let* ((view-constructor (pymacs-eval "sublime.View"))
@@ -21,6 +37,37 @@
 	 (flags (list "-I/home/aparulekar/Developer/GamePlay/gameplay/src" "-I/home/aparulekar/Developer/GamePlay/external-deps/bullet/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/oggvorbis/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/libpng/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/zlib/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/lua/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/glew/include"))
 	 (view (funcall view-constructor (show-file-name) (- position 1) flags))
 	 (query-completions (pymacs-call "getattr" pyclang-scaa "on_query_completions")))
-    (funcall query-completions view "" (list (- position 1)) 'pyclang-receive-completions)))
+    (funcall query-completions view "" (list (- position 1)))))
+
+(defun pyclang-query-completions ()
+  (setq args-list (cons (let* ((cursor-position (what-cursor-position)))
+			  (string-match "point=\\([0-9]+\\)" cursor-position)
+			  (string-to-number (match-string 1 cursor-position))) nil))
+
+  (deferred:$
+    (deferred:next
+      (lambda ()
+	(list "-I/home/aparulekar/Developer/GamePlay/gameplay/src" "-I/home/aparulekar/Developer/GamePlay/external-deps/bullet/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/oggvorbis/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/libpng/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/zlib/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/lua/include" "-I/home/aparulekar/Developer/GamePlay/external-deps/glew/include")))
+    (deferred:nextc it
+      (lambda (flags)
+	(setq args-list (cons flags args-list))
+	(pymacs-eval "sublime.View")))
+    (deferred:nextc it
+      (lambda (view-constructor)
+	(funcall view-constructor
+		 (show-file-name)
+		 (- (car (cdr args-list)) 1) ; position
+		 (car args-list)))) ; flags
+    (deferred:nextc it
+      (lambda (view)
+	(setq args-list (cons view (cons (car (cdr args-list)) nil)))
+	(pymacs-call "getattr" pyclang-scaa "on_query_completions")))
+    (deferred:nextc it
+      (lambda (query-completions)
+	(funcall query-completions
+		 (car args-list) ; view
+		 ""
+		 (list (- (car (cdr args-list)) 1)) ; position
+		 )))))
 
 (provide 'clancs-mode)
