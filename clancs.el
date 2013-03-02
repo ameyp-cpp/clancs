5f8da13a062c798d9eeeb0ca82bd98c124c560b4
Amey Parulekar <amey@wirywolf.com>
2013-03-02 16:53:47 +0530

    Removed separate function for converting relative include path to absolute path. Use a lambda function instead.

--- clancs.el
+++ clancs.el
@@ -10,13 +10,6 @@
 (defun pyclang-receive-completions (completions)
   (insert (prin1-to-string completions)))
 
-(defun get-include-path (include-path)
-  (unless (file-exists-p include-path)
-    (setq-local project-folder (car (car dir-locals-class-alist)))
-    (if (sequencep project-folder)
-	(setq-local project-folder (concat project-folder)))
-    (concat "-I" (symbol-name project-folder) include-path)))
-
 (defun pyclang-query-completions ()
   (deferred:$
     (setq-local file-name (show-file-name))
@@ -24,7 +17,12 @@
     (setq-local position (let* ((cursor-position (what-cursor-position)))
 				     (string-match "point=\\([0-9]+\\)" cursor-position)
 				     (string-to-number (match-string 1 cursor-position))))
-    (setq-local compile-flags (mapcar 'get-include-path
+    (setq-local compile-flags (mapcar (lambda (include-path)
+					(unless (file-exists-p include-path)
+					  (setq-local project-folder (car (car dir-locals-class-alist)))
+					  (if (sequencep project-folder)
+					      (setq-local project-folder (concat project-folder)))
+					  (concat "-I" (symbol-name project-folder) include-path)))
 				   (mapcar (lambda (include-path) (substring include-path 2))
 					   (cdr (assoc 'pyclang-compile-flags
 						       (cdr (assoc 'c++-mode (cdr (car dir-locals-class-alist)))))))))
