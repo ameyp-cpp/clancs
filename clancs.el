ebb5d7aac62ec2e8ad1b5ad0519614530341dc34
Amey Parulekar <amey@wirywolf.com>
2013-03-04 19:03:22 +0530

    Fixed absolute paths in .dir-locals not being handled corrected. Renamed pyclang* to clancs*. Added an ac-source that doesn't do anything.

--- clancs.el
+++ clancs.el
@@ -1,17 +1,19 @@
-(defvar pyclang-path (file-name-directory load-file-name))
+(defvar clancs-path (file-name-directory load-file-name))
+(defvar clancs-candidates (list "A" "B" "C"))
 
-(defun pyclang-init ()
-  (require 'deferred (concat pyclang-path "emacs-deferred/deferred.el"))
-  (require 'concurrent (concat pyclang-path "emacs-deferred/concurrent.el"))
-  (require 'ctable (concat pyclang-path "emacs-ctable/ctable.el"))
-  (require 'epc (concat pyclang-path "emacs-epc/epc.el"))
-  (defvar pyclang-epc (epc:start-epc "python" (list (concat pyclang-path "clancs.py"))))
-  (message "Pyclang initialized."))
+(defun clancs-init ()
+  (require 'deferred (concat clancs-path "emacs-deferred/deferred.el"))
+  (require 'concurrent (concat clancs-path "emacs-deferred/concurrent.el"))
+  (require 'ctable (concat clancs-path "emacs-ctable/ctable.el"))
+  (require 'epc (concat clancs-path "emacs-epc/epc.el"))
+  (setq clancs-epc (epc:start-epc "python" (list (concat clancs-path "clancs.py"))))
+  (message "Clancs initialized."))
 
-(defun pyclang-receive-completions (completions)
+(defun clancs-receive-completions (completions)
   (insert (prin1-to-string completions)))
 
-(defun pyclang-query-completions ()
+;; A sample output:  (("addRef()	void" "addRef()") ("draw()	void" "draw()") ("getMaterial()	gameplay::Material *" "getMaterial()") ("getMesh() const	gameplay::Mesh *" "getMesh()") ("getMeshPartCount() const	unsigned int" "getMeshPartCount()") ("getNode() const	gameplay::Node *" "getNode()") ("getRefCount() const	unsigned int" "getRefCount()") ("getSkin() const	gameplay::MeshSkin *" "getSkin()") ("hasMaterial(unsigned int partIndex) const	bool" "hasMaterial(${1:unsigned int partIndex})") ("release()	void" "release()") ("setMaterial(const char *materialPath)	gameplay::Material *" "setMaterial(${1:const char *materialPath})") ("setMaterial(const char *vshPath, const char *fshPath)	gameplay::Material *" "setMaterial(${1:const char *vshPath}, ${2:const char *fshPath})") ("setMaterial(gameplay::Material *material)	void" "setMaterial(${1:gameplay::Material *material})") ("setNode(gameplay::Node *node)	void" "setNode(${1:gameplay::Node *node})"))
+(defun clancs-query-completions ()
   (deferred:$
     (setq-local file-name (show-file-name))
     (setq-local project-folder (car (car dir-locals-class-alist)))
@@ -19,18 +21,29 @@
 				     (string-match "point=\\([0-9]+\\)" cursor-position)
 				     (string-to-number (match-string 1 cursor-position))))
     (setq-local compile-flags (mapcar (lambda (include-path)
-					(unless (file-exists-p include-path)
-					  (setq-local project-folder (car (car dir-locals-class-alist)))
-					  (if (sequencep project-folder)
-					      (setq-local project-folder (concat project-folder)))
-					  (concat "-I" (symbol-name project-folder) include-path)))
+					(if (file-exists-p include-path)
+					    (concat "-I" include-path)
+					  (progn
+					    (setq-local project-folder (car (car dir-locals-class-alist)))
+					    (if (sequencep project-folder)
+						(setq-local project-folder (concat project-folder)))
+					    (concat "-I" (symbol-name project-folder) include-path))))
 				   (mapcar (lambda (include-path) (substring include-path 2))
-					   (cdr (assoc 'pyclang-compile-flags
+					   (cdr (assoc 'clancs-compile-flags
 						       (cdr (assoc 'c++-mode (cdr (car dir-locals-class-alist)))))))))
 
-    (epc:call-deferred pyclang-epc 'query_completions (list file-name (- position 1) "" compile-flags))
+    (epc:call-deferred clancs-epc 'query_completions (list file-name (- position 1) "" compile-flags))
     (deferred:nextc it
       (lambda (x)
-	(message (concat "Found " (number-to-string (length x)) " completions"))))))
+	(message (prin1-to-string x))))))
+
+(defun ac-clancs-candidates ()
+  clancs-candidates)
+
+(ac-define-source ac-clancs
+  '((candidates . ac-clancs-candidates)
+    (prefix . ac-clancs-prefix)
+    (requires . 0)
+    (cache)))
 
 (provide 'clancs)
