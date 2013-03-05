9f8f645bc5cf7f307201a84ca984bbe1f4c540db
Amey Parulekar <amey@wirywolf.com>
2013-03-05 12:50:39 +0530

    Create popup-items from the list of received completions.

--- clancs.el
+++ clancs.el
@@ -6,31 +6,43 @@
   (require 'concurrent (concat clancs-path "emacs-deferred/concurrent.el"))
   (require 'ctable (concat clancs-path "emacs-ctable/ctable.el"))
   (require 'epc (concat clancs-path "emacs-epc/epc.el"))
+  (require 'popup)
   (setq clancs-epc (epc:start-epc "python" (list (concat clancs-path "clancs.py"))))
   (message "Clancs initialized."))
 
+;(clancs-make-item "foo" "foo(int)" "int") ; int foo(int)
+(defun clancs-make-item (sym signature result-type)
+  (popup-make-item sym :view signature :summary result-type))
+
+(defun clancs-make-item-from-completion (completion)
+  (setq signature (car completion))
+  (string-match "\\([A-Za-z_]+\\).*[\t]\\(.*\\)" signature)
+  (setq sym (match-string 1 signature))
+  (setq result-type (match-string 2 signature))
+  (clancs-make-item sym signature result-type))
+
 ;; Sample input:  (("addRef()	void" "addRef()") ("draw()	void" "draw()") ("getMaterial()	gameplay::Material *" "getMaterial()") ("getMesh() const	gameplay::Mesh *" "getMesh()") ("getMeshPartCount() const	unsigned int" "getMeshPartCount()") ("getNode() const	gameplay::Node *" "getNode()") ("getRefCount() const	unsigned int" "getRefCount()") ("getSkin() const	gameplay::MeshSkin *" "getSkin()") ("hasMaterial(unsigned int partIndex) const	bool" "hasMaterial(${1:unsigned int partIndex})") ("release()	void" "release()") ("setMaterial(const char *materialPath)	gameplay::Material *" "setMaterial(${1:const char *materialPath})") ("setMaterial(const char *vshPath, const char *fshPath)	gameplay::Material *" "setMaterial(${1:const char *vshPath}, ${2:const char *fshPath})") ("setMaterial(gameplay::Material *material)	void" "setMaterial(${1:gameplay::Material *material})") ("setNode(gameplay::Node *node)	void" "setNode(${1:gameplay::Node *node})"))
 (defun clancs-receive-completions (completions)
-  )
+  (setq clancs-candidates (mapcar 'clancs-make-item-from-completion completions)))
 
 (defun clancs-query-completions ()
   (deferred:$
-    (setq-local file-name (show-file-name))
-    (setq-local project-folder (car (car dir-locals-class-alist)))
-    (setq-local position (let* ((cursor-position (what-cursor-position)))
-				     (string-match "point=\\([0-9]+\\)" cursor-position)
-				     (string-to-number (match-string 1 cursor-position))))
-    (setq-local compile-flags (mapcar (lambda (include-path)
-					(if (file-exists-p include-path)
-					    (concat "-I" include-path)
-					  (progn
-					    (setq-local project-folder (car (car dir-locals-class-alist)))
-					    (if (sequencep project-folder)
-						(setq-local project-folder (concat project-folder)))
-					    (concat "-I" (symbol-name project-folder) include-path))))
-				   (mapcar (lambda (include-path) (substring include-path 2))
-					   (cdr (assoc 'clancs-compile-flags
-						       (cdr (assoc 'c++-mode (cdr (car dir-locals-class-alist)))))))))
+    (setq file-name (show-file-name))
+    (setq project-folder (car (car dir-locals-class-alist)))
+    (setq position (let* ((cursor-position (what-cursor-position)))
+		     (string-match "point=\\([0-9]+\\)" cursor-position)
+		     (string-to-number (match-string 1 cursor-position))))
+    (setq compile-flags (mapcar (lambda (include-path)
+				  (if (file-exists-p include-path)
+				      (concat "-I" include-path)
+				    (progn
+				      (setq-local project-folder (car (car dir-locals-class-alist)))
+				      (if (sequencep project-folder)
+					  (setq-local project-folder (concat project-folder)))
+				      (concat "-I" (symbol-name project-folder) include-path))))
+				(mapcar (lambda (include-path) (substring include-path 2))
+					(cdr (assoc 'clancs-compile-flags
+						    (cdr (assoc 'c++-mode (cdr (car dir-locals-class-alist)))))))))
 
     (epc:call-deferred clancs-epc 'query_completions (list file-name (- position 1) "" compile-flags))
     (deferred:nextc it
