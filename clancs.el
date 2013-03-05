1a755869679dde4c6afb91e5f7f021de5311dd6a
Amey Parulekar <amey@wirywolf.com>
2013-03-05 15:40:39 +0530

    It's alive!!!
    Code completions appear to be working. I'm sure there are bugs, but it's working.

--- clancs.el
+++ clancs.el
@@ -1,5 +1,5 @@
 (defvar clancs-path (file-name-directory load-file-name))
-(defvar clancs-candidates (list "A" "B" "C"))
+(setq-local clancs-candidates ())
 
 (defun clancs-init ()
   (require 'deferred (concat clancs-path "emacs-deferred/deferred.el"))
@@ -11,8 +11,8 @@
   (message "Clancs initialized."))
 
 ;(clancs-make-item "foo" "foo(int)" "int") ; int foo(int)
-(defun clancs-make-item (sym signature result-type)
-  (popup-make-item sym :view signature :summary result-type))
+(defun clancs-make-item (sym signature return-type)
+  (popup-make-item sym :view signature :summary return-type))
 
 (defun clancs-make-item-from-completion (completion)
   (setq signature (car completion))
@@ -23,15 +23,18 @@
 
 ;; Sample input:  (("addRef()	void" "addRef()") ("draw()	void" "draw()") ("getMaterial()	gameplay::Material *" "getMaterial()") ("getMesh() const	gameplay::Mesh *" "getMesh()") ("getMeshPartCount() const	unsigned int" "getMeshPartCount()") ("getNode() const	gameplay::Node *" "getNode()") ("getRefCount() const	unsigned int" "getRefCount()") ("getSkin() const	gameplay::MeshSkin *" "getSkin()") ("hasMaterial(unsigned int partIndex) const	bool" "hasMaterial(${1:unsigned int partIndex})") ("release()	void" "release()") ("setMaterial(const char *materialPath)	gameplay::Material *" "setMaterial(${1:const char *materialPath})") ("setMaterial(const char *vshPath, const char *fshPath)	gameplay::Material *" "setMaterial(${1:const char *vshPath}, ${2:const char *fshPath})") ("setMaterial(gameplay::Material *material)	void" "setMaterial(${1:gameplay::Material *material})") ("setNode(gameplay::Node *node)	void" "setNode(${1:gameplay::Node *node})"))
 (defun clancs-receive-completions (completions)
-  (setq clancs-candidates (mapcar 'clancs-make-item-from-completion completions)))
+  (setq clancs-candidates (mapcar 'clancs-make-item-from-completion completions))
+  (ac-start)
+  (ac-update))
 
-(defun clancs-query-completions ()
+(defun clancs-query-completions (prefix &optional position &optional buffer)
   (deferred:$
-    (setq file-name (show-file-name))
+    (setq file-name (buffer-file-name buffer))
     (setq project-folder (car (car dir-locals-class-alist)))
-    (setq position (let* ((cursor-position (what-cursor-position)))
-		     (string-match "point=\\([0-9]+\\)" cursor-position)
-		     (string-to-number (match-string 1 cursor-position))))
+    (unless position
+      (setq position (let* ((cursor-position (what-cursor-position)))
+		       (string-match "point=\\([0-9]+\\)" cursor-position)
+		       (string-to-number (match-string 1 cursor-position)))))
     (setq compile-flags (mapcar (lambda (include-path)
 				  (if (file-exists-p include-path)
 				      (concat "-I" include-path)
@@ -52,9 +55,27 @@
 	    (clancs-receive-completions x))))))
 
 (defun ac-clancs-candidates ()
+  (message (concat "Called. Prefix = " (prin1-to-string ac-prefix) ", Point = " (prin1-to-string ac-point)))
+  ;(clancs-query-completions ac-prefix ac-point ac-buffer)
+  ;; Passing prefix as empty string because ac-point seems to always point to the point (haha)
+  ;; where completion was triggered in the first place.
+  (unless clancs-candidates
+    (clancs-query-completions ac-prefix ac-point ac-buffer))
   clancs-candidates)
 
-(ac-define-source ac-clancs
+(defun ac-clancs-prefix ()
+  (or (ac-prefix-symbol)
+      (let ((c (char-before)))
+	(when (or (eq ?\. c)
+		  ;; ->
+		  (and (eq ?> c)
+		       (eq ?- (char-before (1- (point)))))
+		  ;; ::
+		  (and (eq ?: c)
+		       (eq ?: (char-before (1- (point))))))
+	  (point)))))
+
+(ac-define-source clancs
   '((candidates . ac-clancs-candidates)
     (prefix . ac-clancs-prefix)
     (requires . 0)))
